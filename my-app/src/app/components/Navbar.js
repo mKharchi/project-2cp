9f32e232d9a3df811d1feed2273843d4e07dca1f
mKharchi <kh.merouane.abderrahmane@gmail.com>
2025-02-10 08:29:00 +0100

    "fit-add/added the navbar elements"

--- my-app/src/app/components/Navbar.js
+++ my-app/src/app/components/Navbar.js
@@ -1,21 +1,46 @@
 import Link from "next/link";
 
+import Image from "next/image";
 export default function Navbar() {
   return (
-    <nav className="w-full p-4 bg-gray-100 dark:bg-gray-900 shadow-md">
-      <div className="flex justify-center space-x-6">
-        <Link
-          href="/"
-          className="text-blue-600 dark:text-blue-400 hover:underline text-lg font-semibold"
-        >
-          Home
-        </Link>
-        <Link
-          href="/contact"
-          className="text-blue-600 dark:text-blue-400 hover:underline text-lg font-semibold"
-        >
-          Contact
-        </Link>
+    <nav className="w-full flex justify-center items-center p-0 bg-gray-100 dark:bg-gray-900 shadow-md">
+    <div className="flex items-center justify-between w-full px-3 py-1">
+      {/* Logo */}
+      <div className="rounded-full p-1 flex justify-center items-center">
+        <Image src="/globe.svg" alt="Logo" width={30} height={30} />
+      </div>
+        <div className="flex  justify-between w-[70%]">
+          <Link
+            href="/"
+            className="text-blue-600 dark:text-blue-400 hover:underline text-md font-semibold"
+          >
+            Home
+          </Link>
+          <Link
+            href="/"
+            className="text-blue-600 dark:text-blue-400 hover:underline  text-md font-semibold"
+          >
+             link1
+          </Link>   
+          <Link
+            href="/"
+            className="text-blue-600 dark:text-blue-400 hover:underline  text-md font-semibold"
+          >
+             link2
+          </Link>          
+          <Link
+            href="/"
+            className="text-blue-600 dark:text-blue-400 hover:underline  text-md font-semibold"
+          >
+             link3
+          </Link>          
+          <Link
+            href="/ "
+            className="text-blue-600 dark:text-blue-400 hover:underline  text-md font-semibold"
+          >
+             link4
+          </Link>
+        </div>
       </div>
     </nav>
   );
